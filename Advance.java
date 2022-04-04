97983b5b86fb762b78d55fa8e9f5f89552ac11f2
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2022-04-04 12:59:28 -0400

    Create Advance.java
    This is where all the work is needed to be done because it very obviously doesn't exist

---
+++ Advance.java
@@ -0,0 +1,19 @@
+/*
+Your Name
+Date
+FileName.java
+description
+*/
+
+
+/*
+Write the complete class declaration for the class Advance. Include all necessary instance
+variables and implementations of its constructor and method(s). The constructor should take a
+parameter that indicates the number of days in advance that this ticket is being purchased. Use this 
+price breakdown based on the number of days in advance a ticket is purchased:
+
+* If ticket is purchased less than 6 days in advance- the cost is $44.00 (student advance receives a 25% discount of this cost)
+* However, if ticket is purchased 6 to 10 days (inclusive) in advance - the cost is $40.00 (student advance receives a 25% discount of this cost)
+* But.... if ticket is purchased 11 to 15 days in advance (inclusive) - the cost is $36.00 (student advance receives a 25% discount of this cost)
+* Otherwise, any ticket purchased 16 (inclusive) or more days in advance - the cost is $30.00 (student advance receives a 25% discount of this cost)
+*/
