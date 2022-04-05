dae54f809a452fcded1a55b34bdb8e23f400036c
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2022-04-05 13:17:52 -0400

    Update Advance.java
    Got advance done. Turns out there was a studentAdvance which was for students (I didn't read the instructions) so I'm just gonna add in that file finished cuz I'm too lazy

--- Advance.java
+++ Advance.java
@@ -17,3 +17,29 @@ price breakdown based on the number of days in advance a ticket is purchased:
 * But.... if ticket is purchased 11 to 15 days in advance (inclusive) - the cost is $36.00 (student advance receives a 25% discount of this cost)
 * Otherwise, any ticket purchased 16 (inclusive) or more days in advance - the cost is $30.00 (student advance receives a 25% discount of this cost)
 */
+
+public class Advance extends Ticket {
+
+  private double price;
+
+  public Advance(int d) {
+    
+    if(d < 6) {
+      price = 44.0;
+    }
+    else if((d >= 6) && (d <= 10)) {
+      price = 40.0;
+    }
+    else if((d >= 11) && (d <= 15)) {
+      price = 36.0;
+    }
+    else {
+      price = 30.0;
+    }
+  } //end constructor
+
+  public double getPrice() {
+    return price;
+  }//end getPrice()
+
+}
